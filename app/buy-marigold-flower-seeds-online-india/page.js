50bc20c15e423fb15feb0c0a25716464b6250d75
rajeevmalikjodhpur <rajeevmalikjodhpur@gmail.com>
2026-09-22 10:02:47 +0530

    marrigold seed upload

---
+++ app/buy-marigold-flower-seeds-online-india/page.js
@@ -0,0 +1,124 @@
+export const metadata = {
+    title: "buy marigold flower seeds online india",
+    description: "You can buy marigold flower seeds online from Groway Horticulture. These seeds grow very quickly and produce good-quality flowers."
+  };
+  
+  export default function BuyMarigoldSeeds() {
+    return (
+      <main className="max-w-7xl mx-auto px-4 py-10">
+        <h1 className="text-3xl md:text-4xl font-bold text-center mb-6">buy marigold flower seeds online india</h1>
+        <p className="text-center text-gray-600 mb-10">You can buy marigold flower seeds from Groway Horticulture both online and offline. Groway Horticulture also provides home delivery for marigold seeds.</p>
+        <div className="grid md:grid-cols-2 gap-8 items-start">
+
+  {/* Product Image */}
+  <div className="bg-white rounded-xl shadow-md p-4">
+  <img
+  src="/marry gold seeds.jpg"
+  alt="marry gold seeds online"
+  className="w-full object-contain rounded-lg"
+/>
+  </div>
+
+  {/* Product Details */}
+  <div className="bg-white rounded-xl shadow-md p-6">
+    <h2 className="text-2xl font-bold text-gray-800 mb-3">Marigold Flower Seeds</h2>
+<div className="text-sm text-green-600 font-semibold mb-4">✓ 99.9%  Germination Guarantee</div>
+    <p className="text-gray-600 mb-5">Hight qulity and orignal Seeds</p>
+{/* Price */}
+    <div className="mb-5">
+      <span className="text-3xl font-bold text-green-600">₹50</span>
+      <span className="text-gray-500 ml-2">/ Pack</span>
+    </div>
+
+    {/* Order Buttons */}
+    <div className="grid grid-cols-2 gap-3">
+
+      <a
+        href="https://wa.me/919876543210?text=I want to buy chrysanthemum flower seeds"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-green-500 hover:bg-green-600 text-white
+                   text-center py-3 rounded-lg font-semibold"
+      >
+        WhatsApp Order
+      </a>
+
+      <a
+        href="tel:9876543210"
+        className="bg-blue-600 hover:bg-blue-700 text-white
+                   text-center py-3 rounded-lg font-semibold"
+      >
+        Call to Order
+      </a>
+    </div>
+
+    {/* Product Information */}
+    <div className="border-t mt-6 pt-5 space-y-2 text-sm">
+      <p><strong>Product:</strong>Marigold flowers seeds</p>
+      <p><strong>Germination:</strong>99.9% Plant Germination Guarantee</p>
+      <p><strong>Growth:</strong> Fast Growing</p>
+      <p><strong>Flower Color:</strong> Green</p>
+      <p><strong>Suitable for:</strong> Home Gardening & office , farming Etc</p>
+    </div>
+    {/* Key Features */}
+<section className="mt-8">
+  <h2 className="text-2xl font-bold text-gray-800 mb-4">
+    Key Features
+  </h2>
+
+  <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+    
+    <div className="flex items-center gap-3 p-4 bg-gray-50 rounded-lg border">
+      <div>
+        <p className="text-sm text-gray-500">Plant Type</p>
+        <p className="font-semibold text-gray-800">Indoor And Outdoor</p>
+      </div>
+    </div>
+
+    <div className="flex items-center gap-3 p-4 bg-gray-50 rounded-lg border">
+      <div>
+        <p className="text-sm text-gray-500">Flower</p>
+        <p className="font-semibold text-gray-800">yes</p>
+      </div>
+    </div>
+
+    <div className="flex items-center gap-3 p-4 bg-gray-50 rounded-lg border">
+      <div>
+        <p className="text-sm text-gray-500">Sunlight</p>
+        <p className="font-semibold text-gray-800">3-8 hours</p>
+      </div>
+    </div>
+
+    <div className="flex items-center gap-3 p-4 bg-gray-50 rounded-lg border">
+      <div>
+        <p className="text-sm text-gray-500">Suitable For</p>
+        <p className="font-semibold text-gray-800">
+          Pot, Garden & Balcony , farming , Etc
+        </p>
+      </div>
+    </div>
+
+    <div className="flex items-center gap-3 p-4 bg-gray-50 rounded-lg border">
+      <div>
+        <p className="text-sm text-gray-500">Seed Quantity</p>
+        <p className="font-semibold text-gray-800">
+          300-400 Seeds
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+  </div>
+</div>
+<div className="mt-6">
+<p className="text-gray-600 leading-7">Groway Horticulture's marigold seeds grow very quickly, and flowers appear on the plants in a short time. The flowers have a very pleasant fragrance, and the plants require relatively less care.</p>
+<p className="text-gray-600 leading-7">Anyone can buy these Groway Horticulture seeds. You can use them for growing marigolds in pots as well as for farming.</p>
+<p className="text-gray-600 leading-7">In farming, these seeds can help farmers earn higher profits because they produce flowers very quickly. They also produce a large number of high-quality flowers, which have high demand in the market.</p>  
+  
+  <p className="text-gray-600 leading-7">
+  Groway Horticulture provides you with 100% original, premium-quality marigold flowers seeds to ensure you get the best quality seeds. </p>   
+</div>
+      </main>
+    );
+  }
+  
---
+++ app/marigold-farming-time/page.js
@@ -0,0 +1,47 @@
+export const metadata = {
+    title: "Marigold Farming Time Get Good Profit",
+    description: "Today, we will tell you about the right time for marigold farming**, so that farmers can get more profit."
+  };  
+  export default function MariGoldFarmingTime() {
+    return (
+      <main className="max-w-7xl mx-auto px-4 py-10">
+         <article class="bg-white">
+    <h1 class="text-3xl md:text-4xl font-bold text-gray-900 leading-tight mb-4">
+    Marigold farming time</h1>
+    <p class="text-gray-600 text-lg leading-8 mb-4">Some farmers start marigold farming** at any time, but due to not choosing the right time, they are unable to make a good profit and may face losses. Today, we will tell you the right time for **marigold farming** so that farmers can get a higher profit.
+</p>
+<div class="bg-gray-50 border border-gray-200 rounded-xl p-5 mb-8">
+    <h2 class="text-xl font-bold text-gray-900 mb-4">marigold farming time</h2>
+    <p>There are three best times for marigold farming during which farmers can get higher profits. Let us learn about all three suitable times for marigold farming.</p>
+    <h3 class="font-bold text-gray-900">Marigold is a summer crop</h3>
+    <p>This marigold crop produces flowers in summer. The sowing of this crop is done in January and February. It starts producing flowers after the month of March. Farmers do not get much profit from this crop because the demand for flowers becomes very low during this time.</p>
+
+    <h3 class="font-bold text-gray-900">Marigold rainy-season crop</h3>
+    <p>In the rainy-season crop, farmers can get very high profits. The sowing of this marigold crop is done in June and July, and this crop starts producing flowers in October.</p>
+    <h4>Why does the rainy-season crop give the most profit?</h4>
+    <ul>
+    <li>When we sow the rainy-season crop, the demand for flowers starts increasing after three months.</li>
+    <li>In India, the wedding season starts in November. People buy perfumes for weddings, and many perfumes are made from marigold flowers. Therefore, the price of flowers starts increasing in October.</li>
+    <li>Marigold flowers are widely used for wedding decorations. The price of marigold flowers becomes very high in November.</li>
+    <li>The festival season starts from October. Festivals such as Diwali, Durga Puja, New Year, and Christmas increase the demand for flowers.</li>
+    <li>Marigold flowers are also widely used during elections.</li>
+    </ul>
+    <p><b>Note - </b>These were some of the reasons why the marigold rainy-season crop can provide farmers with higher profits. There are many other reasons why the demand for **marigold flowers** increases, which can also lead to higher prices.
+</p>
+    <h3 class="font-bold text-gray-900">Marigold winter crop</h3>
+<p>The sowing of the marigold winter crop is done between September and October, and the plants start producing flowers by December. During this time, there is good demand for flowers because of the festival and wedding seasons. However, the plants may not produce an abundant number of flowers during this period, so the profit may be slightly lower.</p>
+<p>To earn a good profit from marigold farming, you need to get good-quality flowers from your crop. To grow good-quality flowers, you need to sow good-quality seeds. If you want early, abundant, and beautiful flowers, you can buy marigold seeds from Groway Horticulture.<a href="/buy-marigold-flower-seeds-online-india" class="text-blue-600 hover:underline">Buy Marigold Seeds Online</a>
+</p>
+    <h3 class="font-bold text-gray-900">conclusion</h3>
+    <p>Now you must have understood what the marigold farming time is and which type of marigold farming should be done to earn more profit.</p>
+</div>
+</article>
+<h2 class="text-xl font-semibold mb-3">Related</h2>
+<ul class="list-disc pl-5">
+    <li><a href="/dieffenbachia-plant-buy-online" class="text-blue-600 hover:underline">Buy Dieffenbachia Plant</a></li>
+    <li><a href="/buy-areca-palm-online-india" class="text-blue-600 hover:underline">Buy areca palm plant</a></li>
+</ul>
+      </main>
+    );
+  }
+  
